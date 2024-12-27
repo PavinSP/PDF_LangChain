eb9a5fc78ed193c79b3d79169e12d75081d8ac06
PavinSP <pavinsp122002@gmail.com>
2024-12-27 06:16:40 +0530

    First commit - GUI created successfully

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask questions about your PDFs")
+
+    with st.sidebar:
+        st.subheader("Your PDFs")
+        st.file_uploader("Upload your PDFs here", accept_multiple_files=True)
+        st.button("Process")
+
+if __name__ == "__main__":
+    main()
---
+++ help.md
@@ -0,0 +1,7 @@
+- Install streamlit: to create a Graphical User Interface
+- Install pypdf2: to read pdf files
+- Install langchain: to create a chatbot
+- Install python-dotenv: to load environment variables
+- Install faiss-cpu: to create a vector database
+- Install openai
+- Install huggingface-hub: to use huggingface models
